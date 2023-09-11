1c265f9f81775a6b6f21bdee4c6d8887e42c4efc
Blaise <jetblaise@gmail.com>
2023-09-11 12:58:49 -0400

    feature 2

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 //guess a number between 1 and 10
 const secretNumber = Math.floor(Math.random() * 10) + 1;
+let guess = undefined;
 
 // Prompt
-const guess = parseInt(prompt("Guess a number between 1 and 10"));
-
-if (isNaN(guess)) {
-  alert("Please enter a number");
-} else if (guess < secretNumber) {
-  alert("Too low");
-} else if (guess > secretNumber) {
-  alert("Too high");
-} else if (guess === secretNumber) {
-  alert("Got it!");
+while (guess !== secretNumber) {
+  guess = parseInt(prompt("Guess a number between 1 and 10"));
+  if (isNaN(guess)) {
+    alert("Please enter a number");
+    guess;
+  } else if (guess < secretNumber) {
+    alert("Too low. Guess Again.");
+    guess;
+  } else if (guess > secretNumber) {
+    alert("Too high. Guess again.");
+    guess;
+  } else if (guess === secretNumber) {
+    alert("Got it!");
+  }
 }

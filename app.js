880230b4e8d74ad94eeba58d58a7d82ec3cea6a3
Blaise <jetblaise@gmail.com>
2023-09-11 14:35:05 -0400

    Feature 4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const secretNumber = Math.floor(Math.random() * 10) + 1;
 let guess = undefined;
 let count = 0;
+let previousGuesses = [];
 
 // Prompt
 while (guess !== secretNumber) {
@@ -9,16 +10,32 @@ while (guess !== secretNumber) {
   if (isNaN(guess)) {
     alert("Please enter a number");
     count++;
+    previousGuesses.push(guess);
     guess;
   } else if (guess < secretNumber) {
     alert("Too low. Guess Again.");
     count++;
+    previousGuesses.push(guess);
     guess;
   } else if (guess > secretNumber) {
     alert("Too high. Guess again.");
     count++;
+    previousGuesses.push(guess);
     guess;
   } else if (guess === secretNumber) {
-    alert("It only took you: " + count + " tries but you got it!");
+    let previous_guesses_str = "";
+    for (i = 0; i < previousGuesses.length; i++) {
+      previous_guesses_str += previousGuesses[i];
+      if (i < previousGuesses.length - 1) {
+        previous_guesses_str += ", ";
+      }
+    }
+    alert(
+      "It only took you: " +
+        count +
+        " tries but you got it! You guessed:" +
+        previous_guesses_str +
+        "."
+    );
   }
 }
